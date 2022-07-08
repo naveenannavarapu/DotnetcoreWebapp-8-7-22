2262e61ca4b40b179df1bc97bacf9c975fa1ebee
Naveen <annavarapunaveen>
2022-07-08 17:12:13 +0530

    Add project files.

---
+++ DotnetcoreWebapp-8-7-22/Pages/Index.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace DotnetcoreWebapp_8_7_22.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
